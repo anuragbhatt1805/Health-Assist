41e0238fadbf7becd14e98d731e50ea71f416a0f
Mayank Nandwani <nandwanimayank007@gmail.com>
2023-12-01 14:51:59 +0530

    ele

--- Interface/health-assist/src/components/navb/NavBar.js
+++ Interface/health-assist/src/components/navb/NavBar.js
@@ -1,8 +1,9 @@
-// Header.js
-
 import React, { useState } from 'react';
 import './NavB.css'; // Import the CSS file for styling
 import { Link } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Logo from '../../img/Logo.png' // Import the Logo image
 
 const Header = () => {
   // Example state for user login status
@@ -15,17 +16,26 @@ const Header = () => {
 
   return (
     <header className="header navbar">
-      <div className="logo">Logo</div>
-      <div className="navbar-center">
-        <Link to="/">Home</Link>
-        <Link to="/appointment">Appointment</Link>
-        <Link to="/chat">Chat</Link>
-        <Link to="/faq">FAQ</Link>
+      {/* Logo as an image */}
+      <div className="logo">
+        <img src={Logo} alt="Logo" className="logo-image" />
+      </div>
+      <div className="navbar-center d-flex align-items-center">
+        <Link to="/" className="mx-3">Home</Link>
+        <Link to="/appointment" className="mx-3">Appointment</Link>
+        <Link to="/chat" className="mx-3">Chat</Link>
+        <Link to="/faq" className="mx-3">FAQ</Link>
       </div>
       {isLoggedIn ? (
-        <div>
-          <Link to="/profile">Profile</Link>
-          <button onClick={handleLogout}>Logout</button>
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle dropdown-profile-btn" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+            {/* User icon */}
+            <i className="fas fa-user-circle"></i>
+          </button>
+          <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+            <li><Link to="/profile" className='dropdown-item'>Profile</Link></li>
+            <li><button onClick={handleLogout} className='dropdown-item'>Logout</button></li>
+          </ul>
         </div>
       ) : (
         <Link to="/login">Login / Signup</Link>
