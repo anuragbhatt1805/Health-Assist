c3bab0679c7c3d2f2750c0cd84f77f2ecfdb0854
Anurag Bhatt <anuragbhatt1805@gmail.com>
2023-12-01 11:32:24 +0530

    added me endpoint

--- server/HealthAssist/core/urls.py
+++ server/HealthAssist/core/urls.py
@@ -7,12 +7,13 @@ router = DefaultRouter()
 
 router.register('user', views.ManageUserView)
 router.register('insurance', views.ManageInsuranceView)
-router.register('me', views.ManageProfileView)
+# router.register('me', views.ManageProfileView, basename='me')
 
 app_name = 'core'
 
 urlpatterns = [
     path('signup/', views.CreateUserView.as_view(), name='create'),
     path('login/', views.CreateTokenView.as_view(), name='token'),
+    path('manage/me/', views.ManageProfileView.as_view(), name='profile'),
     path('manage/', include(router.urls)),
 ]
