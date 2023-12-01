d72926beb794474f85af6a19812a5e237a91957c
Yash Raj Saxena <yashrajsaxena1@gmail.com>
2023-12-01 10:40:56 +0530

    Intital DevProfile Card and FAQ pages

---
+++ Interface/health-assist/src/Pages/Faq.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Faqa from '../components/FAQ/FaqSimple'
+import DevProfile from '../components/Developer_profiles/profile_card'
+
+
+function Faq(){
+  return (
+    <div>
+        
+       <Faqa/>
+        <DevProfile/>
+        
+    </div>
+  )
+}
+
+export default Faq
