ade6f4acc754371e170c551ede32d49348f0afc0
Anurag Bhatt <anuragbhatt1805@gmail.com>
2023-12-01 17:53:18 +0530

    remove server skeleton

--- server/HealthAssist/chat/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.conf import settings
-
-from chat.consumers import ChatConsumer
-
-websocket_urlpatterns = [
-    path('ws/chat-rooms/<chat_room_id>/', ChatConsumer),
-]
