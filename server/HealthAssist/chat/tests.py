e9985e436acefb471ef30749f5438c755ecaf27d
Anurag Bhatt <anuragbhatt1805@gmail.com>
2023-12-01 15:14:11 +0530

    chat room implemented

--- server/HealthAssist/chat/tests.py
+++ server/HealthAssist/chat/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from chat.models import ChatMessage
+from mock import Mock
+import json
 
-# Create your tests here.
+class ChatRealtimeTest(TestCase):
+
+    def test_message_broadcasting_and_reception(self):
+        # Create mock objects
+        mock_client = Mock()
+        mock_channel_layer = Mock()
+
+        # Send a message
+        message_content = "Hello, World!"
+        chat_room_id = 1
+        ChatMessage.objects.create(chat_room_id=chat_room_id, sender="test_user", message=message_content)
+
+        # Trigger real-time message broadcasting
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            'chat_room_' + str(chat_room_id),
+            {
+                'type': 'chat_message',
+                'message': message_content,
+                'sender': 'test_user',
+            }
+        )
+
+        # Verify message reception
+        mock_client.receive.assert_called_once_with(json.dumps({
+            'type': 'chat_message',
+            'message': message_content,
+            'sender': 'test_user',
+        }))
--- server/HealthAssist/chat/views.py
+++ server/HealthAssist/chat/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from chat.serializers import *
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+
 
 # Create your views here.
+class ChatRoomViewSet(viewsets.ModelViewSet):
+    queryset = ChatRoom.objects.all()
+    serializer_class = ChatRoomSerializer
+
+
+class ChatRoomMembershipViewSet(viewsets.ModelViewSet):
+    queryset = ChatRoomMembership.objects.all()
+    serializer_class = ChatRoomMembershipSerializer
+
+
+class ChatMessageViewSet(viewsets.ModelViewSet):
+    queryset = ChatMessage.objects.all()
+    serializer_class = ChatMessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Save the message to the database
+        instance = serializer.save()
+
+        # Broadcast the message to the real-time chat component
+        chat_room_id = instance.chat_room_id
+        message = instance.message
+        sender = instance.sender.id
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            'chat_room_' + str(chat_room_id),
+            {
+                'type': 'chat_message',
+                'message': message,
+                'sender': sender,
+            }
+        )
+
+        return serializer.representation
